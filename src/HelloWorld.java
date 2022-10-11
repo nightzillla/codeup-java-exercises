0a4b0b4f2c4a643d9d7b418ab0e1b78c1a9db698
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-11 10:23:36 -0500

    starting java fundamentals

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,20 @@
 // all java code live inside class
 public class HelloWorld {
-    // main tab will give you the following below on line 4
+    // type "main and then press tab" it will give you the following below on line 4
     public static void main(String[] args) {
+        // Declare variable without initializing
+        int weight;
+        String breed;
+
+        // Initializing after declaration
+        weight = 12;
+        breed = "Cavalier King Charles Spaniel";
+        // Initializing and declare at the same time
+        String name = "Grinch";
+        int cutenessFactor = 10;
+        //
+        // type "sout and then press tab"
+        System.out.println("Hello World!");
 
     }
 }
