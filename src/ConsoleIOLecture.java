9bf198b0700dae5ad138a3183e0c4a9d10c77e7c
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-12 09:31:23 -0500

    learning command line

--- src/ConsoleIOLecture.java
+++ src/ConsoleIOLecture.java
@@ -1,6 +1,17 @@
 public class ConsoleIOLecture {
+    // type main and then tab
     public static void main(String[] args) {
         String cohort = "Weywot";
-        System.out.printf("Hello, %s", cohort);
+        String timeOfDay = "morning";
+        byte week = 10;
+        float someNumber = 123.456F;
+        // print format created in the 1970s
+        // line break you need to input this %n
+        System.out.printf("Good, %s %s!%n",cohort, timeOfDay);
+        System.out.printf("This is the %dth week of %s cohort.!%n", week, cohort);
+        System.out.println("Will this be on a separate line?");
+        System.out.println("How about this?");
+        System.out.printf("The number is %11.2f%n", someNumber);
+        System.out.printf("The number is %-11.2f test%n", someNumber);
     }
 }
