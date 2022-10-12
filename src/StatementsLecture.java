541e2a430f0545493aa10240b301fbc012d92b59
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-12 15:20:49 -0500

    learning switch statements

--- src/StatementsLecture.java
+++ src/StatementsLecture.java
@@ -1,6 +1,44 @@
+import java.util.Scanner;
+
 public class StatementsLecture {
+
     public static void main(String[] args) {
         // Statements
         int myInteger = 20;
+        int x = 5;
+        if (x == 5) {
+            System.out.println("x is 5");
+        } else if (x > 5 && x < 15){
+            System.out.println("x is between 5 and 15");
+        } else {
+
+        }
+        String customerChoice;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What flavor do you want?");
+        customerChoice = scanner.next();
+
+        // enhance switch statements
+        switch (customerChoice) {
+            case "vanilla" -> System.out.println("One vanilla coming right up");
+            case "chocolate" -> System.out.println("chocolate coming right up");
+            case "strawberry" -> System.out.println("strawberry coming right up");
+            default -> System.out.println("We don't have that");
+        }
+
+        // traditional switch statements
+//        switch (customerChoice) {
+//            case "vanilla":
+//                System.out.println("One vanilla coming right up");
+//                break;
+//            case "chocolate":
+//                System.out.println("chocolate coming right up");
+//                break;
+//            case "strawberry":
+//                System.out.println("strawberry coming right up");
+//                break;
+//            default:
+//                System.out.println("We don't have that");
+//                break;
     }
 }
