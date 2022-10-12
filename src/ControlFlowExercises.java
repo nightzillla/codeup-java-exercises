a4c06d3b4a89618483d9f12a16c5a7e109fac764
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-12 17:10:03 -0500

    finshed question B

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,8 +1,22 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
+    // Loop Basics
+    // A. While
     // Create an integer variable i with a value of 5.
         int i =  5;
-        while (i < 15);
-        System.out.println("Now I'm in the loop. I has not incremented yet and its value is " + i);
+        while (i < 15) {
+            System.out.println(i);
+            i++;
+        };
+    // B. Do While
+    // Create a do-while loop that will count by 2's starting with 0 and ending at 100.     Follow each number with a new line.
+        int j = 0;
+        do {
+            System.out.println("I am incrementing by two " + j);
+            j+=2;
+        }while(j < 100);
+        // Alter your loop to count backwards by 5's from 100 to -10.
+    // Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
     }
+
 }
