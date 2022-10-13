cfc37c530a082d17416cc133485399c35d503154
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-13 09:05:30 -0500

    starting exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -10,13 +10,23 @@ public class ControlFlowExercises {
         };
     // B. Do While
     // Create a do-while loop that will count by 2's starting with 0 and ending at 100.     Follow each number with a new line.
-        int j = 0;
-        do {
-            System.out.println("I am incrementing by two " + j);
-            j+=2;
-        }while(j < 100);
+//        int j = 0;
+//        do {
+//            System.out.println("I am incrementing by two " + j);
+//            j+=2;
+//        }while(j < 100);
         // Alter your loop to count backwards by 5's from 100 to -10.
+//        int x = 100;
+//        do {
+//            System.out.println("I am decrementing by five " + x);
+//            x-=5;
+//        }while(x >= -10);
     // Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
+        int a = 2;
+        do {
+            System.out.println("I am a square root " + a);
+            a = a*a;
+        }while(a < -1000000);
     }
 
 }
