1f00887b614cf4acf2fc2215b19150a074f2c1f2
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-14 13:46:07 -0500

    starting bark lecture

--- src/MethodsLecture.java
+++ src/MethodsLecture.java
@@ -1,8 +1,3 @@
 public class MethodsLecture {
-    public static void bark() {
-        System.out.println("woof woof!");
-    }
-    public static void main(String[] args) {
 
-    }
 }
--- src/dogLecture.java
+++ src/dogLecture.java
@@ -3,7 +3,16 @@ public class dogLecture {
         System.out.println("woof woof!");
     }
 
+    public static void bark(int weight){
+        if (weight < 25) {
+            System.out.println("Yip yip");
+        } else {
+            System.out.println("Woof woof");
+        }
+    }
     public static void main(String[] args) {
         bark();
+        bark(15);
+        bark(50);
     }
 }
