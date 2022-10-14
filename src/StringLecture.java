da0d958704dc27d8a2152c96e4151d7ca728e2a3
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-14 09:24:11 -0500

    learning hashcode

--- src/StringLecture.java
+++ src/StringLecture.java
@@ -6,8 +6,17 @@ public class StringLecture {
 //        System.out.println(teacher);
         String javier = "Javier";
 
+        System.out.println(Integer.toHexString((System.identityHashCode(javier))));
+
         String alsoJavier = "Javier";
+        System.out.println(Integer.toHexString((System.identityHashCode(alsoJavier))));
 
         javier = javier + " Ruedas";
+        alsoJavier = javier;
+        System.out.println(alsoJavier);
+        System.out.println(javier);
+        System.out.println(Integer.toHexString((System.identityHashCode(javier))));
+        System.out.println(Integer.toHexString((System.identityHashCode(alsoJavier))));
+
     }
 }
