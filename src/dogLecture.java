0ac3ac9b91dd01f5f216c930bb0303553cb43e88
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-14 14:12:04 -0500

    starting methods exercise

--- src/dogLecture.java
+++ src/dogLecture.java
@@ -1,6 +1,8 @@
 public class dogLecture {
-    public static void bark() {
-        System.out.println("woof woof!");
+    public static String bark() {
+//        System.out.println("woof woof!");
+        // if void means nothing returns
+        return "Woof woof!";
     }
 
     public static void bark(int weight){
@@ -10,9 +12,24 @@ public class dogLecture {
             System.out.println("Woof woof");
         }
     }
+
+    public static void bark(int weight, String name){
+        System.out.printf("%s goes ", name);
+        bark(weight);
+//        String output = name + " goes ";
+//        if (weight < 25) {
+//            output += "yip yip";
+//        } else {
+//            output += "woof woof";
+//        }
+//        System.out.println(output);
+    }
     public static void main(String[] args) {
         bark();
+        System.out.println(bark());
         bark(15);
         bark(50);
+        bark(50, "Rudo");
+        bark(15, "Fido");
     }
 }
