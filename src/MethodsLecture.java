dd006583036c9f4839ffe41c6e29681b1d6e4e00
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-14 12:27:10 -0500

    starting dog lecture

---
+++ src/MethodsLecture.java
@@ -0,0 +1,8 @@
+public class MethodsLecture {
+    public static void bark() {
+        System.out.println("woof woof!");
+    }
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/dogLecture.java
@@ -0,0 +1,9 @@
+public class dogLecture {
+    public static void bark() {
+        System.out.println("woof woof!");
+    }
+
+    public static void main(String[] args) {
+        bark();
+    }
+}
