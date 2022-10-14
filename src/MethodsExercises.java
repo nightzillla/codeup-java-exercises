4085d190b136285aa070503867a05a435099d7fa
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-14 14:24:11 -0500

    finshed basic arithemetic

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,5 +1,24 @@
 public class MethodsExercises {
+
     public static void main(String[] args) {
 
+    } // end main
+
+    // Basic Arithmetic
+    // Addition
+    public static int addition(int input, int input2){
+        return input + input2;
     }
-}
+    // Subtraction
+    public static int subtract(int input, int input2){
+        return input - input2;
+    }
+    // Multiplication
+    public static int multiplication(int input, int input2){
+        return input * input2;
+    }
+    // Division
+    public static int division(int input, int input2){
+        return input / input2;
+    }
+} // end MethodsExercise
