eb6e66d5b15d720d7e4ed354184c0c02c9dcfd67
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-14 12:23:07 -0500

    finished bob

--- src/Bob.java
+++ src/Bob.java
@@ -4,14 +4,29 @@ public class Bob {
     public static void main(String[] args) {
 //        Bob is a lackadaisical teenager. In conversation, his responses are very limited.
 //        Bob answers 'Sure.' if you ask him a question. (the input ends with a question mark)
-          Scanner scanner = new Scanner(System.in);
-          System.out.println("Bob are you hungry?");
-          String userAnswer = scanner.next();
-          if (userAnswer.endsWith("?")) {
-              System.out.println("Who, chill out!");
-          }
-//        He answers 'Whoa, chill out!' if you yell at him. (the input ends with an exclamation mark)
-//        He says 'Fine. Be that way!' if you address him without actually saying anything. (empty input)
-//        He answers 'Whatever.' to anything else.
+        //
+//        String goAgain = "y";  // Created this so when the user answer yes it continues
+//        while (goAgain.equalsIgnoreCase("y")) {  // while the user enter yes go again
+//            System.out.println("Hello I am B0B the Human");       // Bob introduction
+//            String userAnswer = scanner.nextLine();
+//            if (userAnswer.endsWith("?")) {
+//                System.out.println("Sure");
+//            } else if (userAnswer.endsWith("!")) {
+//                System.out.println("Whoa chill out");
+//            } else if (userAnswer.equals("")) {
+//                System.out.println("Fine. Be That Way!");
+//            } else if (userAnswer.equalsIgnoreCase("are robots taking over")) {
+//                System.out.println("Of course");
+//            } else if (userAnswer.equalsIgnoreCase("tell a joke")) {
+//                System.out.printf("Why did the chicken cross the road%n.... idk %n");
+//            } else {
+//                System.out.println("WhatEver");
+//            }
+//
+//            System.out.println("Do you want to continue?");
+//            goAgain = scanner.next();
+//            scanner.nextLine();
+//        } // end of while loop
     }
 }
+
