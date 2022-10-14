85146a0507f6ff7ba39142b101cfb38a2aa9ab87
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-14 10:21:01 -0500

    '

--- src/Bob.java
+++ src/Bob.java
@@ -4,6 +4,12 @@ public class Bob {
     public static void main(String[] args) {
 //        Bob is a lackadaisical teenager. In conversation, his responses are very limited.
 //        Bob answers 'Sure.' if you ask him a question. (the input ends with a question mark)
+          Scanner scanner = new Scanner(System.in);
+          System.out.println("Bob are you hungry?");
+          String userAnswer = scanner.next();
+          if (userAnswer.endsWith("?")) {
+              System.out.println("Who, chill out!");
+          }
 //        He answers 'Whoa, chill out!' if you yell at him. (the input ends with an exclamation mark)
 //        He says 'Fine. Be that way!' if you address him without actually saying anything. (empty input)
 //        He answers 'Whatever.' to anything else.
