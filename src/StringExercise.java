42e50dce3015de638d7f7047240c1da4d5f8acfc
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-14 09:46:12 -0500

    finshed question 1

---
+++ src/StringExercise.java
@@ -0,0 +1,23 @@
+public class StringExercise {
+    public static void main(String[] args) {
+//        1.String Basics.
+//        For each of the following output examples: create a String variable that contains the desired output and print it out to the console, you can do this with only one String variable and one print statement for each output example.
+//
+//        String message = "Hello";
+//        System.out.println(message); // prints "Hello"
+//        message = message + " World!";
+//        System.out.println(message); // prints "Hello, World!"
+
+//        We don't need no education
+        String message = "We don't need";
+        System.out.println(message);
+        message = message + " no education";
+        System.out.println(message);
+//        We don't need no thought control
+        message = "We don't need";
+        System.out.println(message);
+        message = message + " no thought control";
+        System.out.println(message);
+
+    } // end of main
+} // end of public class
--- src/StringLecture.java
+++ src/StringLecture.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class StringLecture {
     public static void main(String[] args) {
 //        String teacher = "teacher";
@@ -17,6 +19,18 @@ public class StringLecture {
         System.out.println(javier);
         System.out.println(Integer.toHexString((System.identityHashCode(javier))));
         System.out.println(Integer.toHexString((System.identityHashCode(alsoJavier))));
+
+
+        String letsTryThis = "Javier";
+        System.out.println(Integer.toHexString((System.identityHashCode(letsTryThis))));
+        String theOriginalJavier = "Javier";
+        System.out.println(letsTryThis == theOriginalJavier);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What's your name?");
+        String userName = scanner.next();
+        System.out.println(theOriginalJavier == userName);
+        System.out.println(theOriginalJavier.equals(userName));
 
     }
 }
