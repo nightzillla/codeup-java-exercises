22194d9c6d80921159688f672af5770d645cae38
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-14 09:11:00 -0500

    starting string lecture

--- src/StringLecture.java
+++ src/StringLecture.java
@@ -1,7 +1,13 @@
 public class StringLecture {
     public static void main(String[] args) {
-        String teacher = "teacher";
-        teacher = "not teacher";
-        System.out.println();
+//        String teacher = "teacher";
+//        System.out.println(teacher);
+//        teacher = "not teacher";
+//        System.out.println(teacher);
+        String javier = "Javier";
+
+        String alsoJavier = "Javier";
+
+        javier = javier + " Ruedas";
     }
 }
