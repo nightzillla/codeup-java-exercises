f584cf0622b22c4ab2ee2122200c7051b1334ea7
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-16 18:16:03 -0500

    learning constructor

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,19 @@
 public class Person {
+    public static void main(String[] args) {
+
+    }
+    private String name;
+    public String getName(){
+//TODO: return the person's name
+        return this.name;
+    }
+    public void setName(String name){
+//TODO: change the name field to the passed value
+        this.name = name;
+    }
+    public void sayHello(){
+//TODO: print a message to the console using the person's name
+        System.out.println(name);
+    }
+
 }
