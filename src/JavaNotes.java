e36565c8f461d0f6ba6f57aca09df84325d2326a
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-17 09:27:36 -0500

    learning primitive types

--- src/JavaNotes.java
+++ src/JavaNotes.java
@@ -3,4 +3,17 @@ public class JavaNotes {
     //You can think of a class as a template, or a blueprint, for objects that will be created based on it.
 
     // Data - In computer science, data is information that is stored and processed by a computer.
+
+    // Java classifies different pieces of data with data types based on their value
+    // A data type provides a set of possible values
+    // If a piece of data is one of a data type's values, it is classified as that data type.
+
+    // Primitive Types are: Boolean, int, double, char
+
+    // Boolean: represents a true or false value
+    // Int: represents a whole number
+    // Double: represents a decimal number
+    // Char" represents a single letter or symbol
+
+
 }
