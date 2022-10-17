d7dda4685a4d0c95ee56a6f2d170d683b0356241
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-17 16:13:51 -0500

    learning static

---
+++ src/MathTest.java
@@ -0,0 +1,7 @@
+public class MathTest {
+    public static void main(String[] args) {
+        int testInt = -23;
+        int absValTest = Math.abs(testInt);
+        System.out.println(absValTest);
+    }
+}
--- src/Quote.java
+++ src/Quote.java
@@ -8,7 +8,7 @@ public class Quote {
             case 2 : return "Pura vida";
             case 3 : return "Knowledge is power";
             case 4 : return "With great power comes great responsibility";
-            default: return " When torrential water tosses boulders, it is because of its momentum; when the strike of a hawk breaks the body of its prey, it is because timing. Thus the momentum of one skilled in war is overwhelming, their attack precisely regulated. Their potential is that of a fully draw crossbow; their timing, the release of the tigger.";
+            default: return " When torrential water tosses boulders, it is because of its momentum; when the strike of a hawk breaks the body of its prey, it is because timing. Thus the momentum of one skilled in war is overwhelming, their attack precisely regulated. Their potential is that of a fully draw crossbow; their timing, the release of the trigger.";
         }
     }
 }
