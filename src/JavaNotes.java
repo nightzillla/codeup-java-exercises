8a85bcf21c4961e18452571a46eb60e17d971b0a
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-17 09:23:11 -0500

    starting notes

--- src/JavaNotes.java
+++ src/JavaNotes.java
@@ -1,4 +1,6 @@
 public class JavaNotes {
     // A class is used to define a custom type in Java.
     //You can think of a class as a template, or a blueprint, for objects that will be created based on it.
+
+    // Data - In computer science, data is information that is stored and processed by a computer.
 }
--- src/util/Input.java
+++ src/util/Input.java
@@ -1,4 +1,10 @@
 package util;
 
+import java.util.Scanner;
+
 public class Input {
+
+    private Scanner input = new Scanner(System.in);
+
+
 }
