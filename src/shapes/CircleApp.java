d8959d2e5ca22bf938a19f29cb29ee0caee410f3
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-19 10:46:42 -0500

    learning java array

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -5,6 +5,6 @@ public class CircleApp {
     public static void main(String[] args) {
         Input input = new Input();
         Circle circle = new Circle(input.getDouble("Enter the radius of the circle:"));
-        System.out.println(circle.getArea());
+        System.out.printf("The area of your cirlce is %.2f and its circumference is %.2f and its circumference is %.2f", circle.getArea(),circle.getCircumference());
     }
 }
