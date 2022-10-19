2f32fd5e9cb3653e067c1e5ff3e6b7d48f8509c7
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-19 10:55:45 -0500

    learning array sort

--- src/ArrayLecture.java
+++ src/ArrayLecture.java
@@ -1,2 +1,17 @@
+import java.util.Arrays;
+
 public class ArrayLecture {
+    public static void main(String[] args) {
+        int[] numbers;
+        numbers = new int[5];
+        numbers[0] = 78;
+        numbers[1] = 253;
+        numbers[2] = -23;
+        numbers[3] = -789;
+        numbers[4] = 36;
+        System.out.println(numbers[0]);
+        Arrays.sort(numbers);
+        System.out.println(numbers[0]);
+    }
+
 }
