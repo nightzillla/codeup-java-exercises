b835cde7ac36706a3b8cf8e63ff29b7deb98cc43
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-20 18:15:18 -0500

    inheritance and polymorphism

---
+++ src/shapes/Square.java
@@ -0,0 +1,24 @@
+package shapes;
+    // square inherits rectangle because of extends
+public class Square extends Rectangle{
+
+
+    // this is default constructor
+    public Square(){};
+    // this is custom constructor
+    public Square(int side){
+        // super means I want my parents thing
+        super.setWidth(side);
+        super.setLength(side);
+    };
+    public int getArea(){
+        return (int) Math.pow(getLength(), 2);
+    }
+    public int getPerimeter(){
+        return 4 * getLength();
+    }
+
+
+
+
+}
