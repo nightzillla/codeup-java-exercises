de483ba2577e23cd97c0651610c9ea9977df17c3
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-20 13:39:41 -0500

    bird lecture

--- src/Cars/Vehicle.java
+++ src/Cars/Vehicle.java
@@ -2,6 +2,7 @@ package Cars;
 
 public class Vehicle {
     //    Create a Vehicle class with two properties: a name instance variable and a makeNoise() method. The instance variable should be private and have getters and setters (in IntelliJ try cmd-N). The makeNoise method should just sout out a typical vehicle noise. Then create a more specific vehicle subclass, such as Motorcycle, Car, Truck, Tractor, whatever. Make the specific vehicle class extend the Vehicle class. Otherwise, keep it empty. Create a test class, perhaps calling it VehicleTest. Instantiate the specific vehicle, assign a value to the name property and run the methods on the specific vehicle. Notice the inherited method works and the inherited name property is assignable on the subclass.
+    // this is an instance
     private String name;
     public String getName() {
         return name;
@@ -13,6 +14,5 @@ public class Vehicle {
 
     public void makeNoise() {
         System.out.println("vroom vroom");
-
     }
 }
---
+++ src/animals/Parrot.java
@@ -0,0 +1,9 @@
+package animals;
+
+public class Parrot extends BirdLecture {
+
+    public void makeNoise(){
+        System.out.println("When torrential water tosses boulders, it is because of its momentum; when the strike of a hawk breaks the body of its prey, it is because timing. Thus the momentum of one skilled in war is overwhelming, their attack precisely regulated. Their potential is that of a fully draw crossbow; their timing, the release of the trigger.");
+    }
+
+}
