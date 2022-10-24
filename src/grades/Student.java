b2274ab09f478298fca51b1f9bd4a10338ffba05
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-24 16:28:05 -0500

    student constructor

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -1,6 +1,17 @@
 package grades;
 
+import rpg.Monster;
+
+import java.util.ArrayList;
+
 public class Student {
     private String name;
-    private double grades;
+    private ArrayList<Integer>grades;
+    public Student(){};
+    // Custom Constructor
+    public Student(String name) {
+        this.name = name;
+        this.grades = new ArrayList<>();
+    }
+
 }
