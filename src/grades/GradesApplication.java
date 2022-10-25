2af08daa93b3970fdd9c3b6882e0d24406556f41
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-25 12:04:15 -0500

    edit grades

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -8,21 +8,21 @@ import java.util.Scanner;
 public class GradesApplication {
     public static void main(String[] args) {
         Student Danny = new Student("Danny");
-        Danny.addGrade(50);
-        Danny.addGrade(70);
-        Danny.addGrade(30);
         Student Steven = new Student("Steven");
+        Student Rod = new Student("Rod");
+        Student Mason = new Student("Mason");
+        Danny.addGrade(90);
+        Danny.addGrade(70);
+        Danny.addGrade(60);
         Steven.addGrade(90);
         Steven.addGrade(99);
         Steven.addGrade(100);
-        Student Rod = new Student("Rod");
-        Steven.addGrade(88);
-        Steven.addGrade(95);
-        Steven.addGrade(92);
-        Student Mason = new Student("Mason");
-        Steven.addGrade(80);
-        Steven.addGrade(91);
-        Steven.addGrade(55);
+        Rod.addGrade(88);
+        Rod.addGrade(95);
+        Rod.addGrade(92);
+        Mason.addGrade(80);
+        Mason.addGrade(91);
+        Mason.addGrade(55);
         HashMap<String, Student> Students = new HashMap<>();
         Scanner scanner = new Scanner(System.in);
         Students.put("trickzillla", Danny);
@@ -42,14 +42,14 @@ public class GradesApplication {
             userNameInput = scanner.nextLine();
             if (Students.containsKey(userNameInput)) {
                 Student newStudentLookUp = Students.get(userNameInput);
-                System.out.printf("Name: %s - GitHub UserName:%s%nCurrent Average: %s%n", newStudentLookUp, userNameInput, newStudentLookUp.getGradeAverage());
+                System.out.printf("Name: %s - GitHub UserName:%s%nCurrent Average: %.2f%n", newStudentLookUp, userNameInput, newStudentLookUp.getGradeAverage());
             } else {
-                System.out.printf("Sorry, no student found with the GitHub username of ", userNameInput);
+                System.out.printf("Sorry, no student found with the GitHub username of %s ", userNameInput);
             }
             System.out.println("Would you like to see another student? y/n");
             userAnswer = scanner.nextLine();
         } while (userAnswer.equals("y"));
-        if (userAnswer.equals("n")) {
+            if (userAnswer.equals("n")) {
             System.out.println("Goodbye, and have a wonderful day!");
         }
     }
