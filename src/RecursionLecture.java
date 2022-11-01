1557243ca562ea2c73362b7fd81061e9a999bdab
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-11-01 15:51:12 -0500

    finshed lecture

--- src/RecursionLecture.java
+++ src/RecursionLecture.java
@@ -1,3 +1,23 @@
 public class RecursionLecture {
+    // What is recursion?
+    // A function that calls itself
+    // What is a recursive problem? Or wht kinds of problems can recursion solve
+    // A problem whose definition( or answer or solution) is expressed in smaller terms of itself
+    // Note watch videos over recursion.
+
+    // n factorial (or n!) is the product of all positive integers less than or equal to n
+
+    // Analysis of an example: 5! = 5 x 4 x 3 x 2 x 1
+    // What do you notice about the above description of 5!?
+    // It can be rewritten as 5! = 5 x 4!, or 5 x 4 x 3!
+    // Thus: n! = n x (n-1)!
+
+    // Enter fibonacci
+    // fib(n) = fib(n-1) + fib(n-2)
+    // fib(0) = 0 and fib(1) = 1
+
+    // Write the recursive definition of an algorithm to find the minimum value in an array of ints
+    // Express the definition as the 2 hallmarks
+
 
 }
